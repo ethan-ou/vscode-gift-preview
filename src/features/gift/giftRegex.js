f9c4ada83302fac4bdf07bfce028a4da4fc61bdd
ethan-ou <ethantim@gmail.com>
2020-01-29 14:46:35 +1100

    First draft of regex matching for GIFT

--- src/features/gift/giftRegex.js
+++ src/features/gift/giftRegex.js
@@ -1,8 +1,34 @@
 export default function(text) {
-    let document = text.split(/\r?\n/)
-        .map(comment => comment.replace(/(^[ \\t]+)?(([\W\n]|^)(\/\/)+).+/g, ''))
-        .map(category => category.replace(/(^[ \\t]+)?(((^|\n)\s*[$]CATEGORY:))(.+)/g, `<b>$5</b>`))
-        .map(title => title.replace(/\s*(::)\s*(.*?)(::)/g, `<b>$2</b><br>`));
-    console.log(document);
+    let document = removeBackslash(lineRegex(documentRegex(text))).split(/\r?\n/)
     return document.map(i => `<p>${i}<p>`).join('');
+}
+
+function documentRegex(text) {
+    const newLineAnswer = /([^\\]|[^\S\r\n][^=])(=|~)/g
+    const correctAnswer = /([^\\]|^{)(([^\\]|^|\\s*)=(.*)(?=[=~}]|\\n))/g;
+    const incorrectAnswer = /([^\\]|^{)(([^\\]|^|\\s*)~(.*)(?=[=~}]|\\n))/g;
+
+    return text
+    .replace(newLineAnswer, `\n$2`)
+    .replace(correctAnswer, `$1<li style="color: green">$4</li>`)
+    .replace(incorrectAnswer, `$1<li style="color: red">$4</li>`)
+}
+
+function lineRegex(text) {
+    return text
+    .split(/\r?\n/)
+    .map(comment => comment.replace(/(^[ \\t]+)?(([\W\n]|^)(\/\/)+).+/g, ''))
+    .map(category => category.replace(/(^[ \\t]+)?(((^|\n)\s*[$]CATEGORY:))(.+)/g, `<br><b>$5</b><br>`))
+    .map(title => title.replace(/\s*(::)\s*(.*?)(::)/g, `<br><b>$2</b><br>`))
+    .map(openBracket => openBracket.replace(/([^\\]|^){([#])?/g, `$1<br>`))
+    .map(closeBracket => closeBracket.replace(/([^\\]|^)}/g, `$1<br>`))
+    .join('')
+}
+
+function removeBackslash(text) {
+    return text.split(/\r?\n/)
+    .map(colon => colon.replace(/[\\]:/g, ":"))
+    .map(openBracket => openBracket.replace(/[\\]{/g, "{"))
+    .map(closeBracket => closeBracket.replace(/[\\]}/g, "}"))
+    .join('')
 }
--- src/features/gift/index.ts
+++ src/features/gift/index.ts
@@ -3,6 +3,7 @@ import giftRegex from "./giftRegex";
 import parser from "./parser";
 
 export default function(text: string): string {
-    const validation = parser.parse(text);
-    return validation.type === "result" ? giftHTML(validation.result) : giftRegex(text);
+    //const validation = parser.parse(text);
+    //validation.type === "result" ? giftHTML(validation.result) : giftRegex(text);
+    return giftRegex(text);
 }
