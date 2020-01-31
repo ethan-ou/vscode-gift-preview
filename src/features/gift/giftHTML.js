ed5de1ac4a395ee006c785c29cfed7b6d3930cfc
ethan-ou <ethantim@gmail.com>
2020-02-01 10:45:32 +1100

    Updated Markup of GIFT HTML and added hashing for input names

--- src/features/gift/giftHTML.js
+++ src/features/gift/giftHTML.js
@@ -1,4 +1,5 @@
 import marked from "marked";
+import Hashes from "jshashes";
 
 export default function giftPreviewHTML(questions) {
   if (!questions) return ``;
@@ -31,22 +32,21 @@ function sortQuestionType(q) {
       return `
         ${makeTitle("Multiple choice", q.title)}
         <p>${formatText(q.stem)}</p>
-        ${formatAnswers(q.choices)}
+        ${formatAnswers(q.choices, q.type)}
         ${formatFeedback(q.globalFeedback)}
         `;
     case "Numerical":
-      console.log("numerical!", q);
       return `
         ${makeTitle("Numerical", q.title)}
         <p>${formatText(q.stem)}</p>
-        ${formatNumerical(q.choices)}
+        ${formatAnswers(q.choices, q.type)}
         ${formatFeedback(q.globalFeedback)}
       `;
     case "Short":
       return `
         ${makeTitle("Short answer", q.title)}
         <p>${formatText(q.stem)}</p>
-        ${formatAnswers(q.choices)}
+        ${formatAnswers(q.choices, q.type)}
         ${formatFeedback(q.globalFeedback)} 
         `;
     case "Essay":
@@ -57,8 +57,15 @@ function sortQuestionType(q) {
     case "TF":
       return `
         ${makeTitle("True/False", q.title)}
-        <p><em>("${q.isTrue ? "True" : "False"}")</em>
-        ${formatText(q.stem)}</p>
+        <p>${formatText(q.stem)}</p>
+        ${formatAnswers(
+          {
+            isTrue: q.isTrue,
+            correctFeedback: q.correctFeedback,
+            incorrectFeedback: q.incorrectFeedback
+          },
+          q.type
+        )}
         ${formatFeedback(q.globalFeedback)}
         `;
     case "Matching":
@@ -77,64 +84,152 @@ function makeTitle(type, title) {
   return `<b>${type} ${conditionalDisplay(title, `" ${title} "`)}</b>`;
 }
 
-function formatAnswers(choices) {
+function formatAnswers(choices, type) {
   let result = ``;
-  for (let choice of choices) {
-    result += `
-    <li class="${choice.isCorrect ? "correct" : "wrong"}">
-    ${conditionalDisplay(choice.weight, `<em>(${choice.weight}%)</em>`)} 
-    ${formatText(choice.text)}
-    ${choice.feedback !== null ? ` [${formatText(choice.feedback)}]` : ``}
-    </li>
-    `;
-  }
-  return `<ul>${result}</ul>`;
-}
+  
+  const hash = new Hashes.MD5();
+  
+  if (type === "MC") {
+    let numberOfCorrect = 0;
+    choices.forEach(choice => (choice.isCorrect ? numberOfCorrect++ : null));
+    let type = numberOfCorrect > 1 ? "checkbox" : "radio";
 
-function formatNumerical(choices) {
-  function numericalAnswer(choice) {
-    switch (choice.type) {
-      case "simple":
-        return `${choice.number}`;
-      case "range":
-        return `${choice.number} ± ${choice.range}`;
-      case "high-low":
-        return `${choice.numberHigh} - ${choice.numberLow}`;
-      default:
-        return;
-    }
-  }
 
-  if (Array.isArray(choices)) {
-    let result = ``;
+    result += `Select one${type === "checkbox" ? " or more:" : ":"}`;
+
     for (let choice of choices) {
       result += `
-      <li class="${choice.isCorrect ? "correct" : "wrong"}">
-      <p>${conditionalDisplay(choice.weight, `<em>(${choice.weight}%)</em>`)} 
-      ${numericalAnswer(choice.text)}
-      ${choice.feedback !== null ? ` [${formatText(choice.feedback)}]` : ``}</p>
-      </li>
+      <div class="custom-input">
+        <input type="${type}" name=${hash}>
+          <label class="${
+            choice.isCorrect ? "correct" : "wrong"
+          } for="${hash}">
+          ${conditionalDisplay(choice.weight, `<em>(${choice.weight}%)</em>`)}
+          ${formatText(choice.text)} 
+          ${choice.feedback !== null ? ` [${formatText(choice.feedback)}]` : ``}
+        </label>
+        </input>
+      </div>
+    `;
+    }
+  }
+
+  if (type === "Short") {
+    let choiceText = choices.map(choice => formatText(choice.text));
+    result += `
+      <div>
+        Answer: <input type="text" placeholder="${choiceText.join(", ")}">
+      </div>
+    `;
+
+    // for (let choice of choices) {
+    //   result += `
+    //   <p>
+    //   ${conditionalDisplay(choice.weight, `<em>(${choice.weight}%)</em>`)}
+    //   ${formatText(choice.text)}
+    //   ${choice.feedback !== null ? ` [${formatText(choice.feedback)}]` : ``}
+    //   </p>
+    //   `;
+    // }
+  }
+
+  if (type === "TF") {
+    const isTrue = choices && choices.isTrue;
+    const correctFeedback = choices && choices.correctFeedback;
+    const incorrectFeedback = choices && choices.incorrectFeedback;
+
+    const feedback = choice =>
+      choice !== null ? ` [${formatText(choice)}]` : ``;
+
+    result += `Select one:`;
+    result += `
+      <div class="custom-input">
+        <input type="radio" name="${hash}">
+          <label class="${choices.isCorrect ? "correct" : "wrong"} for="${hash}">
+            True ${
+              isTrue ? feedback(correctFeedback) : feedback(incorrectFeedback)
+            }
+          </label>
+        </input>
+      </div>
+
+      <div class="custom-input">
+      <input type="radio" name="${hash}">
+        <label class="${choices.isCorrect ? "correct" : "wrong"} for="${hash}">
+          False ${
+            !isTrue ? feedback(correctFeedback) : feedback(incorrectFeedback)
+          }
+        </label>
+      </input>
+    </div>
+    `;
+  }
+
+  if (type === "Numerical") {
+    function numericalAnswer(choice) {
+      switch (choice.type) {
+        case "simple":
+          return `${choice.number}`;
+        case "range":
+          return `${choice.number} ± ${choice.range}`;
+        case "high-low":
+          return `${choice.numberLow} - ${choice.numberHigh}`;
+        default:
+          return;
+      }
+    }
+
+    if (Array.isArray(choices)) {
+      let choiceText = choices.map(choice => numericalAnswer(choice.text));
+
+      result = `
+        <div>
+          Answer: <input type="text" placeholder="${choiceText.join(", ")}">
+        <div>
+        `;
+    } else {
+      result = `
+      <div>
+        Answer: <input type="text", placeholder="${numericalAnswer(choices)}">
+      </div>
       `;
     }
-    return `<ul>${result}</ul>`;
-  } else {
-    return `
-    <ul>
-      <li><p>${numericalAnswer(choices)}</p></li>
-    </ul>`;
   }
+
+  return `${result}`;
 }
 
 function formatMatching(matchPairs) {
   let result = ``;
-  for (let match of matchPairs) {
-    result += `
-    <li>
-      ${formatText(match.subquestion)} => ${match.subanswer}
-    </li>
+
+  let subAnswers = `
+     <select class="custom-select">
+      <option value="" disabled selected hidden>Choose...</option>
+      ${matchPairs
+        .map(
+          match => `
+        <option value=${match.subanswer}>
+          ${match.subanswer}
+        </option>`
+        )
+        .join("")}
+    </select>
     `;
-  }
-  return `<ul>${result}<ul>`;
+
+    for (let match of matchPairs) {
+      result += `
+      <tr>
+        <td style="padding-right: 1rem">
+        ${formatText(match.subquestion)} 
+        </td>
+        <td>
+        ${subAnswers}
+        </div>
+        </td>
+      </tr>
+      `;
+    }
+    return `<table><tbody>${result}</tbody></table>`;
 }
 
 function formatText(giftText) {
