c3964591244d37f7f414c349f02a2933406f2d5c
ethan-ou <ethantim@gmail.com>
2020-02-03 11:43:20 +1100

    Code cleanup for GIFTParser

--- src/features/gift/index.ts
+++ src/features/gift/index.ts
@@ -1,114 +1,143 @@
 import diff from "deep-diff";
-import eol from 'eol';
+import eol from "eol";
 import giftRegex from "./giftRegex";
 import giftHTML from "./giftHTML";
 import parser from "./parser";
 
-// TODO: Fix undefined for one of the questions in giftHTML!
+interface ParseResult {
+  type: string;
+  result: any;
+}
 
 export default class GIFTParser {
   public text: string;
   public html: string;
-  private blankLines: any[];
-  private finalText: any[];
   private lineNumbers: number[];
+  private blankLines: string[];
+  private finalText: string[];
 
-  constructor(text = '') {
-    const cleanText = eol.lf(text);
+  constructor(text = "") {
+    const cleanText: string = eol.lf(text);
     this.text = cleanText;
     this.lineNumbers = this.generateLineNumbers(cleanText);
-   
+
     this.blankLines = this.findBlankLines(cleanText);
     this.finalText = this.parseLines(this.blankLines);
-   
-    this.html = this.finalText.join('\n\n');
+
+    this.html = this.generateCodeLines(this.finalText, this.lineNumbers).join(
+      "\n\n"
+    );
   }
 
-  public updateText(text: string) {
-    const cleanText = eol.lf(text);
+  public updateText(text: string): void {
+    const cleanText: string = eol.lf(text);
     this.text = cleanText;
     this.lineNumbers = this.generateLineNumbers(cleanText);
 
     const newBlankLines = this.findBlankLines(cleanText);
-    this.finalText = this.diffLines(this.blankLines, newBlankLines, this.finalText);
-    
+    this.finalText = this.diffLines(
+      this.blankLines,
+      newBlankLines,
+      this.finalText
+    );
+
     this.blankLines = newBlankLines;
-    this.html = this.finalText.join('\n\n');
+    this.html = this.generateCodeLines(this.finalText, this.lineNumbers).join(
+      "\n\n"
+    );
   }
 
-  private findBlankLines(text: string) {
+  public getHTML() {
+    return this.html;
+  }
+
+  private findBlankLines(text: string): string[] {
     return text.split("\n\n");
   }
 
-  private parseLines(splitLines: any) {
-    let finalText = [];
-    for (let i=0; i < splitLines.length; i++) {
-      finalText = this.generateHTML(splitLines[i], i, finalText, this.lineNumbers[i]);
+  private parseLines(splitLines: any): string[] {
+    let finalText: string[] = [];
+    for (let i = 0; i < splitLines.length; i++) {
+      finalText = this.generateHTML(splitLines[i], i, finalText);
     }
     return finalText;
   }
 
-  private diffLines(oldLines: any, newLines: any, finalText: any) {
-    const diffArray = diff(oldLines, newLines);
+  private diffLines(
+    oldLines: string[],
+    newLines: string[],
+    finalText: string[]
+  ): string[] {
+    const diffArray: any[] = diff(oldLines, newLines);
     if (!diffArray) {
       return finalText;
     }
 
-    let finalTextCopy = finalText;
+    // console.log(diffArray);
+    let finalTextCopy: string[] = Array.from(finalText);
 
-    for (let i=0; i<diffArray.length; i++) {
+    for (let i = 0; i < diffArray.length; i++) {
       let diff = diffArray[i];
-
       let diffKind = (diff && diff.kind && diff.kind[0]) || (diff && diff.kind);
-      
+
       // If the change was an Edit
       if (diffKind === "E") {
-        finalTextCopy = this.generateHTML(diff.rhs, diff.path[0], finalTextCopy, this.lineNumbers[diff.path[0]]);
+        finalTextCopy = this.generateHTML(
+          diff.rhs,
+          diff.path[0],
+          finalTextCopy
+        );
       }
 
       // If the change was an Array change
       if (diffKind === "A") {
         if (diff.item.kind === "N") {
-          finalTextCopy = this.generateHTML(diff.item.rhs, diff.index, finalTextCopy, this.lineNumbers[diff.index]);
+          finalTextCopy = this.generateHTML(
+            diff.item.rhs,
+            diff.index,
+            finalTextCopy
+          );
         }
         if (diff.item.kind === "D") {
           finalTextCopy.splice(diff.index, 1);
         }
-        
       }
     }
 
     return finalTextCopy;
   }
 
-   private generateHTML(text: string, index: number, array: any[], line?: number) {
-    let parsedLine = parser.parse(text);
-    
+  private generateHTML(text: string, index: number, array: string[]): string[] {
+    let parsedLine: ParseResult = parser.parse(text);
+    const tempArray: string[] = Array.from(array);
+
     if (parsedLine.type === "result") {
-      array[index] = `<div class="code-line" data-line="${line}">${giftHTML(parsedLine.result)}</div>`;
+      tempArray[index] = giftHTML(parsedLine.result);
     }
     if (parsedLine.type === "error") {
-      array[index] = `<div class="code-line" data-line="${line}">${giftRegex(text)}</div>`;
+      tempArray[index] = giftRegex(text);
     }
 
-    return array;
-   }
+    return tempArray;
+  }
 
-   private generateLineNumbers(text: string) {
-    const lineNumbers: any[] = [];
-    const prev: any[] = [];
-    
-    const lines = text.split("\n\n").map(line => {
-      prev.push(line.split("\n").length);
-      return line.split("\n").length;
-    });
+  private generateLineNumbers(text: string): number[] {
+    const lineNumbers: number[] = [];
+    const lines = text.split("\n\n").map(line => line.split("\n").length);
 
-    let memory = 0;
-    for (let i = 0; i < lines.length; i++) {
-      memory += lines[i] + 1;
-      lineNumbers.push(memory - prev[i]);
+    let memory = -1;
+    for (let line of lines) {
+      memory += line + 1;
+      lineNumbers.push(memory - line);
     }
 
-     return lineNumbers.map(line => line - 1);
-   }
+    return lineNumbers;
+  }
+
+  private generateCodeLines(array: string[], lines: number[]): string[] {
+    return array.map(
+      (item, index) =>
+        `<div class="code-line" data-line="${lines[index]}">${item}</div>`
+    );
+  }
 }
