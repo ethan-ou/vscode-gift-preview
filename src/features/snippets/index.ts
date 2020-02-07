9d96b4aa010a89072741c78be3f7f173d7f4a92c
ethan-ou <ethantim@gmail.com>
2020-02-07 11:02:38 +1100

    Updated snippets table

--- src/features/snippets/index.ts
+++ src/features/snippets/index.ts
@@ -18,51 +18,56 @@ const snippets = `
     <p>To get started, type one of the following snippets and press <code>ENTER</code>:</p>
     <table>
         <thead>
-        <tr>
-        <th align="right">Prefix</th>
-        <th>Description</th>
-        </tr>
+            <tr>
+                <th align="right">Prefix</th>
+                <th>Description</th>
+            </tr>
         </thead>
-        <tbody><tr>
-        <td align="right"><code>multiple choice question</code></td>
-        <td><code>Multiple choice question with four options.</code></td>
-        </tr>
-        <tr>
-        <td align="right"><code>multiple choice question multiple</code></td>
-        <td><code>Multiple choice question with multiple answers.</code></td>
-        </tr>
-        <tr>
-        <td align="right"><code>matching question</code></td>
-        <td><code>Matching question with four options.</code></td>
-        </tr>
-        <tr>
-        <td align="right"><code>short answer question</code></td>
-        <td><code>Short answer question with one correct answer.</code></td>
-        </tr>
-        <tr>
-        <td align="right"><code>missing word question</code></td>
-        <td><code>Missing word question with three options.</code></td>
-        </tr>
-        <tr>
-        <td align="right"><code>true false question</code></td>
-        <td><code>True-False question.</code></td>
-        </tr>
-        <tr>
-        <td align="right"><code>essay question</code></td>
-        <td><code>Essay question.</code></td>
-        </tr>
-        <tr>
-        <td align="right"><code>numerical question</code></td>
-        <td><code>Numerical question.</code></td>
-        </tr>
-        <tr>
-        <td align="right"><code>category</code></td>
-        <td><code>Category label.</code></td>
-        </tr>
-        <tr>
-        <td align="right"><code>category nested</code></td>
-        <td><code>Nested category label.</code></td>
-        </tr>
+        <tbody>
+            <tr>
+                <td align="right"><code>multiple choice question</code></td>
+                <td><code>Multiple choice question with four options.</code></td>
+            </tr>
+            <tr>
+                <td align="right"><code>multiple choice question multiple</code></td>
+                <td><code>Multiple choice question with multiple answers.</code></td>
+            </tr>
+            <tr>
+                <td align="right"><code>matching question</code></td>
+                <td><code>Matching question with four options.</code></td>
+            </tr>
+            <tr>
+                <td align="right"><code>short answer question</code></td>
+                <td><code>Short answer question with one correct answer.</code></td>
+            </tr>
+            <tr>
+                <td align="right"><code>missing word question</code></td>
+                <td><code>Missing word question with three options.</code></td>
+            </tr>
+            <tr>
+                <td align="right"><code>true false question</code></td>
+                <td><code>True-false question.</code></td>
+            </tr>
+            <tr>
+                <td align="right"><code>essay question</code></td>
+                <td><code>Essay question.</code></td>
+            </tr>
+            <tr>
+                <td align="right"><code>numerical question</code></td>
+                <td><code>Numerical question.</code></td>
+            </tr>
+            <tr>
+                <td align="right"><code>title</code></td>
+                <td><code>Question title.</code></td>
+            </tr>
+            <tr>
+                <td align="right"><code>category</code></td>
+                <td><code>Category label.</code></td>
+            </tr>
+            <tr>
+                <td align="right"><code>category nested</code></td>
+                <td><code>Nested category label.</code></td>
+            </tr>
         </tbody>
     </table>
 </div>
