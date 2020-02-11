e784b0148554c042448476a6f3c96fa26d125ae5
ethan-ou <ethantim@gmail.com>
2020-02-11 13:25:33 +1100

    Remove Font-Awesome and inline css

--- src/features/gift/giftHTML.js
+++ src/features/gift/giftHTML.js
@@ -258,9 +258,11 @@ function formatFeedback(feedback) {
 }
 
 function formatAnswerIcon(correct) {
+  const faCheck = `<svg class="icon" style="display: inline-block; width: 1em; color: #5cb85c;" role="img" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>`
+  const faTimes = `<svg class="icon" style="display: inline-block; width: 0.75em; color: #d9534f;" role="img" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>`
   return correct
-    ? `<i class="fas fa-check icon" style="color: #5cb85c"></i>`
-    : `<i class="fas fa-times icon" style="color: #d9534f"></i>`;
+    ? faCheck
+    : faTimes;
 }
 
 function conditionalDisplay(variable, output) {
